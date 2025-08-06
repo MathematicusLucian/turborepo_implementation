6961c4f6c52c9cb619dca12217acd54552666273
MathematicusLucian <52294958+MathematicusLucian@users.noreply.github.com>
2025-08-06 05:01:33 +0100

    More api rroutes for Hono

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,40 +4,52 @@ import { serve } from '@hono/node-server'
 import { cors } from 'hono/cors'
 import { zValidator } from '@hono/zod-validator';
 import { db, schema } from '@repo/db-adapter';
-import { add } from '@repo/db-adapter/add';
-import { createUserService } from '@repo/db-adapter/services/users';
+import { newUserService } from '@repo/db-adapter/services/users';
 import { usersInsertSchema } from '@repo/db-adapter/schema/users';
+import postgres from "postgres";
 
 const app = new Hono();
 app.use('/api/*', cors())
-const { getUsers, getUserById, createUser } = createUserService(db);
+
+const databaseConnection = (env: any) => {
+    const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB, PG_BOUNCER_PORT } = env;
+    const poolConnectionString = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${PG_BOUNCER_PORT}/${POSTGRES_DB}?sslmode=disable`;
+    const dbAdapter = db(poolConnectionString);
+    return newUserService(dbAdapter);
+}
 
 app.get('/', (c) => {
-    // const result = dbAdapter.query('SELECT * FROM users');
-    // return c.json(result);
-    return c.json({ data: add(1, 2) })
+    return c.json({ message: 'Use the /api prefix' })
 });
 
-app.get('/api/users', async (c) => {
+app.get('/api', (c) => {
+    return c.json({ message: 'Welcome to the API' })
+});
+
+app.get('/api/users', async (c: any) => {
+    const { getUsers, getUserById, createUser } = databaseConnection(c.env);
     const users = await getUsers();
-    return c.json(users)
+    return c.json(users);
 });
 
-// app.post('/api/users', zValidator('json', usersInsertSchema), async (c) => {
-//   // const data = c.req.valid('json')
-//   // const [user] = await createUser(data);
-//   // return c.json(user)
-//   return c.json({})
-// });
+app.get('/api/users/:id', async (c) => {
+    const { getUsers, getUserById, createUser } = databaseConnection(c.env);
+    const user = await getUserById(c.req.param('id'));
+    return c.json(user)
+});
 
-// app.get('/api/users/:id', async (c) => {
-//   const [user] = await getUserById(c.req.param('id'));
-//   return c.json(user)
-// });
+app.post('/api/users', zValidator('json', usersInsertSchema), async (c) => {
+    const data = c.req.valid('json')
+    const { getUsers, getUserById, createUser } = databaseConnection(c.env);
+    const user = await createUser(data);
+    return c.json(user)
+});
 
 app.get('/api/posts', async (c) => {
-    const posts = await db.select().from(schema.posts);
-    return c.json(posts)
+    //     const { getUsers, getUserById, createUser } = databaseConnection(c.env);
+    //     const posts = await db.select().from(schema.posts);
+    //     return c.json(posts)
+    return c.json({})
 });
 
 export default app;
