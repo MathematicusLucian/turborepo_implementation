da4df44f8d8573804c46892acea7f89e857848b7
MathematicusLucian <52294958+MathematicusLucian@users.noreply.github.com>
2025-08-06 12:00:31 +0100

    Tidied ElectricSql TsConfig, and migrations

---
+++ apps/electric-next/src/initDb.ts
@@ -0,0 +1,55 @@
+
+// import { createPgLiteClient, PgDialect, frontMigrations } from "../front";
+// import { PGliteInterface } from "@electric-sql/pglite";
+
+// type TablesToSync = {
+//   shape?: string;
+//   table: string;
+//   primaryKey?: string[];
+// }[]
+
+// const TablesToSync: TablesToSync = [{
+//   table: 'users',
+//   primaryKey: ['id'],
+// }]
+
+// export async function runMigrations(pg: PGliteInterface, dbName: string) {
+//   const db = createPgLiteClient(pg);
+
+//   // Prevent running of multiple schema migrations 
+//   let isLocalDBSchemaSynced = false;
+
+//   console.log("isLocalDBSchemaSynced", isLocalDBSchemaSynced);
+//   if (!isLocalDBSchemaSynced) {
+//     const start = performance.now();
+//     try {
+//       await new PgDialect().migrate(
+//         frontMigrations,
+//         //@ts-ignore
+//         db._.session,
+//         dbName
+//       );
+//       isLocalDBSchemaSynced = true;
+//       console.info(`✅ Local database ready in ${performance.now() - start}ms`);
+//     } catch (cause) {
+//       console.error("❌ Local database schema migration failed", cause);
+//       throw cause;
+//     }
+//   }
+
+//   return db;
+// }
+
+// export async function syncTables(pg: PGliteInterface, electricBaseUrl: string) {
+//   const syncStart = performance.now();
+//   await Promise.all(TablesToSync.map(({ shape, table, primaryKey }) => {
+//     //@ts-ignore
+//     pg.electric?.syncShapeToTable({
+//       shape: { url: `${electricBaseUrl}/${shape || table}` },
+//       table,
+//       primaryKey: primaryKey ?? ['id'],
+//     });
+//   }));
+
+//   console.info(`✅ Local database synced in ${performance.now() - syncStart}ms`);
+// }
